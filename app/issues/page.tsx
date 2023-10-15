a5572e18573e65c15f03522750fa1b9a0e76b260
Kwaku Boateng <kwakuboateng.dev@gmail.com>
2023-10-15 02:09:38 +0000

    setup radix ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Button } from '@radix-ui/themes'
 
 const IssuesPage = () => {
   return (
-    <div>IssuesPage</div>
+    <div>
+      <Button>New Issue</Button>
+    </div>
   )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import './globals.css'
+import '@radix-ui/themes/styles.css';
+import { Theme } from '@radix-ui/themes';
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navbar from './Navbar'
@@ -18,8 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        <main>{children}</main>
+        <Theme>
+          <Navbar />
+          <main>{children}</main>
+        </Theme>
       </body>
     </html>
   )
