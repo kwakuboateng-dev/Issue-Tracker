cfe3c198e6816ff07c125f98b01d857924c34dc3
Kwaku Boateng <kwakuboateng.dev@gmail.com>
2023-10-17 04:53:25 +0000

    Error component created

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -9,6 +9,7 @@ import { useState } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { createIssueSchema } from '@/app/validationSchemas';
 import { z } from 'zod';
+import ErrorMessage from '@/app/components/ErrorMessage';
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
@@ -41,7 +42,7 @@ const NewIssuePage = () => {
           <TextField.Input placeholder='Title' {...register('title')} />
         </TextField.Root>
 
-        {errors.title && <Text color='red' as="p">{errors.title.message}</Text>}
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
 
         <Controller
           name='description'
@@ -55,7 +56,7 @@ const NewIssuePage = () => {
           )}
         />
 
-        {errors.description && <Text color='red' as="p">{errors.description.message}</Text>}
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
 
         <Button>Submit New Issue</Button>
       </form>
