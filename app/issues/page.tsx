9f6b612f4d50e356a0a985888408116dba181d04
Kwaku Boateng <kwakuboateng.dev@gmail.com>
2023-10-17 13:31:10 +0000

    Custom Link component

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Button, Table } from '@radix-ui/themes'
-import Link from 'next/link'
+import { Table } from '@radix-ui/themes'
 import prisma from '@/prisma/client'
 import IssuesStatusBadge from '../components/IssuesStatusBadge'
 import delay from 'delay'
 import IssueActions from './IssueActions'
+import Link from '../components/Link'
 
 
 const IssuesPage = async () => {
